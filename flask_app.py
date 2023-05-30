c36a9d64b2ff05805df13f1737cf8f6684fd2fc8
Joyce Claessen <J.claessenoomen@gmail.com>
2023-05-30 20:32:46 +0200

    basic Flask

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app=Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return "<p> Hello world</p>"
+    return "<p>This is my world now!</p>"
+
+
+if __name__=="__main__":
+    app.run (port=5000,debug=True)
+
+
